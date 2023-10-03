9200d3104c0bb36c631fe74792a637e0b6475473
Taisei <taisei96.web@gmail.com>
2023-10-04 08:58:20 +0900

    API実装

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
-function callApi() {
-  const res = fetch("https://jsonplaceholder.typicode.com/users");
-  console.log(res);
+// function callApi() {
+//   const res = fetch("https://jsonplaceholder.typicode.com/users");
+//   console.log(res);
+// }
+
+// callApi();
+
+async function callApi() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  console.log(users);
 }
 
 callApi();
